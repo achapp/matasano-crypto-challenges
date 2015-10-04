06e4c7611156db603fdd65d1b4e1cc9885222f9d
Adam Chappell <adamdchappell@gmail.com>
2015-10-03 22:38:55 -0500

    Initial commit
    Edited gitignore with the github visual studio suggested file. Set up
    Basics project and unit testing. First two challenges solved.

---
+++ Basics/Basics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatasanoCrypto
+{
+    public static class Basics
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static String HexToBase64(String hexstring)
+        {
+            return Convert.ToBase64String(StringToByteArray(hexstring));
+        }
+
+        public static String XOR (String stringA, String stringB)
+        {
+            byte[] byteStringA = StringToByteArray(stringA);
+            byte[] byteStringB = StringToByteArray(stringB);
+            byte[] xorred = new byte[byteStringA.Length];
+
+            for (int x = 0; x < byteStringA.Length; x++)
+            {
+                xorred[x] = (byte)(byteStringA[x] ^ byteStringB[x]);
+            }
+
+            return ByteArrayToString(xorred);
+        }
+
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                     .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            string hex = BitConverter.ToString(ba);
+            return hex.Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ Tests/CryptoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatasanoCrypto;
+
+namespace MatasanoCrypto
+{
+    [TestClass]
+    public class CryptoTests
+    {
+        [TestMethod]
+        public void TestHexToBase64()
+        {
+            String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            Assert.AreEqual("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", Basics.HexToBase64(hex));
+        }
+
+        [TestMethod]
+        public void TestXOR()
+        {
+            String stringA = "1c0111001f010100061a024b53535009181c";
+            String stringB = "686974207468652062756c6c277320657965";
+            Assert.AreEqual("746865206b696420646f6e277420706c6179", Basics.XOR(stringA, stringB));
+        }
+    }
+}
